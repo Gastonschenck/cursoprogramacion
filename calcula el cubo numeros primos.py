89543d4ce04eb2f6ef49bba4045488d2cd470a60
Gastonschenck <166716731+Gastonschenck@users.noreply.github.com>
2024-06-13 10:26:44 -0300

    Create calcula el cubo numeros primos.py

---
+++ calcula el cubo numeros primos.py
@@ -0,0 +1,10 @@
+#cuales y cuantos numeros primos comprendidos entre 1 y 1000
+count = 0
+for num in range(2, 1001):
+    for i in range(2, num):
+        if (num % i) == 0:
+            break
+    else:
+        count += 1
+        print ("el cubo de: ",num, " es: ", num**3)
+print(count)
